cc8f51793b1fda02980896ae9b04cb2e99a09c64
Ravi Kumar Singh <rksingh@isystango.com>
2019-03-14 15:14:40 +0530

    aliasing ransack

--- app/models/participant_program.rb
+++ app/models/participant_program.rb
@@ -1,4 +1,6 @@
 class ParticipantProgram < ApplicationRecord
   belongs_to :program, optional: true
   belongs_to :participant, class_name: 'User', optional: true
+  ransack_alias :participant, :participant_first_name_or_participant_last_name
+  ransack_alias :program, :program_name
 end
