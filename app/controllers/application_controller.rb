28add1f2674062dce178fcb62c2de73df27fdffe
Ravi Kumar Singh <rksingh@isystango.com>
2019-03-18 12:35:51 +0530

    added sign out link and redirection after auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.json { head :forbidden, content_type: 'text/html' }
+      format.html { redirect_to main_app.root_url, notice: exception.message }
+      format.js   { head :forbidden, content_type: 'text/html' }
+    end
+  end
+  
+  def after_sign_in_path_for(resource)
+    if resource.admin?
+      admin_dashboard_path
+    else
+      root_path
+    end
+  end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
+
 end
