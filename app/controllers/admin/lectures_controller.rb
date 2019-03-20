9b910de34d85194ec96437327a0526188adacad9
Twinkle Verma <tverma@isystango.com>
2019-03-20 19:53:06 +0530

    added full calendar for lecture

--- app/controllers/admin/lectures_controller.rb
+++ app/controllers/admin/lectures_controller.rb
@@ -4,8 +4,16 @@ class Admin::LecturesController < AdminController
   before_action :make_time, only: :make_time
 
   def index
+
     @program = Program.find(params[:program_id])
-    @lectures = @program.lectures.page(params[:page]).per(10)
+
+    if ActiveModel::Type::Boolean.new.cast(params[:calender])
+      @lectures = @program.lectures
+      calender_data
+      render 'lecture_calender'
+    else
+      @lectures = @program.lectures.page(params[:page]).per(10)
+    end
   end
 
   def show; end
@@ -29,5 +37,19 @@ class Admin::LecturesController < AdminController
     def make_time
       lecture_params[:begin_time] = DateTime.parse lecture_params[:begin_time].split("/").join(' (IST)')
       lecture_params[:end_time] = DateTime.parse lecture_params[:end_time].split("/").join(' (IST)')
+    end
+
+    def calender_data
+      lecture_arr = []
+      @lectures.each do |lecture|
+        lecture_event = {}
+        lecture_event['id'] = lecture.id
+        lecture_event['title'] = lecture.title
+        lecture_event['title'] = lecture.title
+        lecture_event['start'] = lecture.begin_time
+        lecture_event['end'] = lecture.end_time
+        lecture_arr << lecture_event
+      end
+      gon.push({ events: lecture_arr })
     end
 end
